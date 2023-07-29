a2ca824eb15558017eab53c9e1aaf0e6ba953e93
sunaarun <sanaharun91@gmail.com>
2023-07-29 11:58:38 +0200

    Face Detecting Project

--- processing_images/detect_faces.py
+++ processing_images/detect_faces.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 from processing_images.loadingImages import loadImages
 
 def detectFace():
-    myImages= loadImages()
+    myImages= loadImages() #Load the images and store in "myImages" List
     for im in myImages:
-        gray=pre_process(im)
-        im2=process_gray_image(gray,im)
-        plt.figure(figsize=(20, 10))
+        gray=pre_process(im) #Convert the input image into gray
+        im2=loadClassifier(gray, im) #Start processing the input image
+        plt.figure(figsize=(20, 10)) #plot a figure and show the output
         plt.imshow(im2)
         plt.waitforbuttonpress()
         plt.axis('off')
@@ -16,7 +16,7 @@ def pre_process(img):
     gray_img= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     return gray_img
 
-def process_gray_image(gray,img):
+def loadClassifier(gray, img): # to Load a Classifier and start detecting faces
   face_classifier=cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
   face = face_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
   for (x, y, w, h) in face:
